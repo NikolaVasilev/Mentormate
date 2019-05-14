8e86220246255fb6b27428525d2be1b36a4f0a16
NikolaVasilev <nikola@madcorp.info>
2019-05-14 23:11:52 +0300

    Adding TaskMM Solution01 and Solution02

---
+++ Solution01/Run.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static Solution01.Validations;
+
+namespace Solution01
+{
+    public class Run
+    {
+        static void Main()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+
+
+            while (!IsInputCorrect(n))
+            {
+                Console.WriteLine("Input must be number in range 2 - 10000 and must be odd");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            //print
+            Print(n);
+
+        }
+
+        private static void Print(int n)
+        {
+
+            for (int i = 0; i <= n / 2; i++)
+            {
+                Console.WriteLine(new string('-', n - i)
+                                  + new string('*', n + i * 2)
+                                  + new string('-', n - i * 2)
+                                  + new string('*', n + i * 2)
+                                  + new string('-', n * 2 - i * 2)
+                                  + new string('*', n + i * 2)
+                                  + new string('-', n - i * 2)
+                                  + new string('*', n + i * 2)
+                                  + new string('-', n - i));
+            }
+            for (int i = 0; i <= n / 2; i++)
+            {
+                Console.WriteLine(new string('-', n / 2 - i)
+                                  + new string('*', n)
+                                  + new string('-', 1 + i * 2)
+                                  + new string('*', n + (n - i * 2) - 1)
+                                  + new string('-', i * 2 + 1)
+                                  + new string('*', n)
+                                  + new string('-', (n / 2 - i) * 2)
+                                  + new string('*', n) 
+                                  + new string('-', 1 + i * 2)
+                                  + new string('*', n + (n - i * 2) - 1) 
+                                  + new string('-', i * 2 + 1)
+                                  + new string('*', n)
+                                  + new string('-', (n / 2 - i)));
+            }
+        }       
+    }
+}
---
+++ Solution02/Run.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using static Solution02.Validations;
+
+namespace Solution02
+{
+    public class Run
+    {
+        static void Main()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+
+
+            while (!IsInputCorrect(n))
+            {
+                Console.WriteLine("Input must be number in range 2 - 10000 and must be odd");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            //print
+            Print(n);
+
+        }
+
+        private static void Print(int n)
+        {
+
+            for (int i = 0; i <= n / 2; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < n - i * 2; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < n * 2 - i * 2; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < n - i * 2; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < n - i; j++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i <= n / 2; i++)
+            {
+                for (int j = 0; j < n / 2 - i; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < 1+i *2 ; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + (n - i * 2 - 1); j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < i*2 + 1; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < (n / 2 - i) *2; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < 1 + i*2; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n + (n - i * 2) - 1; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < i + 1 + i; j++)
+                {
+                    Console.Write('-');
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < n / 2 - i; j++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
